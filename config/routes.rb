5b2b946e5b953afe9af4caaf3d53e5af6f840b29
Rafael1981 <r.a.ferreira1981@gmail.com>
2015-07-24 20:40:22 +1000

    Adding admin model and customizing devise for 2 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins , controllers: { sessions: "admin/sessions" }
+  devise_for :users , controllers: { sessions: "users/sessions" }
   resources :logs
 
   get 'pages/about'
