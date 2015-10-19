ec08934baa7674c34b37adbaf8ba104421f4fdf6
Rafael1981 <r.a.ferreira1981@gmail.com>
2015-10-19 23:55:52 +1100

    finishing report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,23 @@
 class ReportsController < ApplicationController
 
 	def select
+		if current_user.admin?
+			@users = User.all.order("Firstname ASC")
+		else
+		end
 		
 	end
 
 	def generate
+
+		@mindate = params[:mindate]
+		@maxdate = params[:maxdate]			
+		@userselected = params[:post][:person_id]
+		if current_user.admin?
+			@logs = Log.where(user: @userselected).order("Signin DESC")
+		else
+			@logs = Log.where(user: current_user).order("Signin DESC")
+		end
 		
 	end
 
