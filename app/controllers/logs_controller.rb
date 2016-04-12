b67c79182a68a9c6f6102f8d0f2db7c57ca7d1f4
Rafael1981 <r.a.ferreira1981@gmail.com>
2016-04-13 07:41:34 +1000

    fixing bonus update and views

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -221,7 +221,7 @@ class LogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def log_params
-      params.require(:log).permit(:signin, :signout)
+      params.require(:log).permit(:signin, :signout, :bonus)
     end
 
     def shifts_today
